096d41d5378578abefbd1b4693a39662b5ca8203
Rashed Al Ghailani <Authority217@gmail.com>
2024-12-16 15:16:27 +0400

    Adding DTOs for Product and ProductFilter

---
+++ DTO/ProductFilterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagementSystem.DTO
+{
+    public class ProductFilterDto
+    {
+        public string Name { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
