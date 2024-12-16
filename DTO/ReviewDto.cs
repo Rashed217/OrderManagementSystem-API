29df91c2045ca55606348b249c3e5481f47721a4
Rashed Al Ghailani <Authority217@gmail.com>
2024-12-16 15:20:07 +0400

    Added DTO for Reviews

---
+++ DTO/ReviewDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderManagementSystem.DTO
+{
+    public class ReviewDto
+    {
+        public int ProductId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
