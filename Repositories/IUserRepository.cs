20e03dc5a62452803e06b26be68946a40400ae33
Rashed Al Ghailani <Authority217@gmail.com>
2024-12-17 10:02:51 +0400

    Added functionalities to register users and add them to the database

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using OrderManagementSystem.Model;
+
+namespace OrderManagementSystem.Repositories
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+        User GetByEmail(string email);
+        User GetById(int id);
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using OrderManagementSystem.DTO;
+using OrderManagementSystem.Model;
+using OrderManagementSystem.Repositories;
+
+namespace OrderManagementSystem.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepo;
+        private readonly IConfiguration _configuration;
+
+        public UserService(UserRepository userRepo, IConfiguration configuration)
+        {
+            _userRepo = userRepo;
+            _configuration = configuration;
+        }
+
+        public User Register(RegisterDto model)
+        {
+            var existingUser = _userRepo.GetByEmail(model.Email);
+            if (existingUser != null)
+            {
+                throw new Exception("Email is already registered.");
+            }
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Password = model.Password,
+                Phone = model.Phone,
+                Role = model.Role,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            _userRepo.AddUser(user);
+            return user;
+        }
+    }
+}
